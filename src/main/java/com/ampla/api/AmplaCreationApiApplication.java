ef5daa064ff0a8fbb119bf0cae2f1d2d95ad2a2c
Jean Pierre L.Peterson <jeanpierrepeterson01@gmail.com>
2022-08-16 15:36:41 -0400

    My First Commit

---
+++ src/main/java/com/ampla/api/AmplaCreationApiApplication.java
@@ -0,0 +1,13 @@
+package com.ampla.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplaCreationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmplaCreationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ampla/api/AmplaCreationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampla.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmplaCreationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
